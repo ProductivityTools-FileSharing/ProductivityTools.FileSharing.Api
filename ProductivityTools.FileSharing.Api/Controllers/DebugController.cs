15ffc0f79604e0b7244e0a753d7b9a25ba97b6ec
Pawel Wujczyk <pwujczyk@gmail.com>
2025-06-29 13:19:22 +0200

    debug controller

---
+++ ProductivityTools.FileSharing.Api/Controllers/DebugController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductivityTools.FileSharing.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DebugController : Controller
+    {
+
+        public DebugController()
+        {
+        }
+
+        [HttpGet]
+        [Route("Date")]
+        public string Date()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        [HttpGet]
+        [Route("AppName")]
+        public string AppName()
+        {
+            return "PTFileSharing";
+        }
+
+        [HttpGet]
+        [Route("Hello")]
+        public string Hello(string name)
+        {
+            return string.Concat($"Hello {name.ToString()} Current date:{DateTime.Now}");
+        }
+
+        [HttpGet]
+        [Route("ServerName")]
+        public string ServerName()
+        {
+            string server = "NoSQLServer";
+            return server;
+        }
+    }
+
+}
