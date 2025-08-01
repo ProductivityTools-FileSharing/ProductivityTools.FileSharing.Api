290e9c4f420d9180f91ba17ef87b71860fdd9651
Pawel Wujczyk <pwujczyk@gmail.com>
2025-08-01 21:36:11 +0200

    cors

--- ProductivityTools.FileSharing.Api/Program.cs
+++ ProductivityTools.FileSharing.Api/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                                   builder =>
                                   {
-                                      builder.WithOrigins("http://localhost:3000", "https://ptfilesharing-45459715128.us-central1.run.app").AllowAnyHeader().AllowAnyMethod();
+                                      builder.WithOrigins("http://localhost:3000", "https://ptfilesharing-45459715128.us-central1.run.app", "https://ptservicestatus-309299231472.us-central1.run.app/").AllowAnyHeader().AllowAnyMethod();
                                   });
 });
 builder.Services.AddAuthentication(options =>
